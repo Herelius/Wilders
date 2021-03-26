1a434b5b8b3bcecea0ce90efa4bbe65550ac5885
Heranca <heranca.kamalo@gmail.com>
2021-03-26 01:32:07 +0100

    final version

--- script.js
+++ script.js
@@ -3,24 +3,36 @@
 // 1.1.Joueurs//
 const cpuImage = document.querySelector('.cpuImageChoice');
 const newPlayer = document.querySelector('#playerName');
+const playerImage = document.querySelector('.playerImageChoice');
 
 // 1.2.Bouttons//
-const allButtons = document.querySelector('#threeChoice');
-const rockButton = document.querySelector('.rockChoice');
+const buttonsSelector = document.querySelectorAll('#threeChoice button');
+
+/*const rockButton = document.querySelector('.rockChoice');
 const paperButton = document.querySelector('.paperChoice');
-const scissorsButton = document.querySelector('.scissorsChoice');
+const scissorsButton = document.querySelector('.scissorsChoice');*/
 
 // 1.3.Jeu//
-const cpuChoiceValue = require('./cpu.js');
-let playerChoiceValue;
+
+const imgOfTheSelection = {
+  rock:
+    'https://www.pierrefeuilleciseaux.fr/wp-content/uploads/sites/5/2013/10/pierre-300x300.png',
+  paper:
+    'https://www.pierrefeuilleciseaux.fr/wp-content/uploads/sites/5/2013/10/feuille-300x300.png',
+  scissors:
+    'https://www.pierrefeuilleciseaux.fr/wp-content/uploads/sites/5/2013/10/ciseaux-300x300.png',
+};
+
+const whatCpuSelected = require('./cpu.js');
 let roundNumber = 0;
 let playerScore = 0;
 let cpuScore = 0;
 const score = document.querySelector('#results');
 const roundInGame = document.querySelector('#round');
+const maximumROund = 5;
 
-// 1.3.a.Fonctions du jeu//
-const winner = () => {
+//Fonctions//
+const theWinnerIs = () => {
   if (playerScore > cpuScore) {
     return `The winner is ${newPlayer.innerHTML} with a score of ${playerScore}-${cpuScore}`;
   }
@@ -29,88 +41,58 @@ const winner = () => {
   }
 };
 
-const cpuChoiceValueImg = () => {
-  if (cpuChoiceValue() === 'rock') {
-    return (cpuImage.src =
-      'https://www.pierrefeuilleciseaux.fr/wp-content/uploads/sites/5/2013/10/pierre-300x300.png');
-  }
-  if (cpuChoiceValue() === 'paper') {
-    return (cpuImage.src =
-      'https://www.pierrefeuilleciseaux.fr/wp-content/uploads/sites/5/2013/10/feuille-300x300.png');
-  }
-  if (cpuChoiceValue() === 'scissors') {
-    return (cpuImage.src =
-      'https://www.pierrefeuilleciseaux.fr/wp-content/uploads/sites/5/2013/10/ciseaux-300x300.png');
+const cpuSelection = () => {
+  const cpuButtonSelected = whatCpuSelected();
+
+  cpuImage.src = imgOfTheSelection[cpuButtonSelected];
+
+  return cpuButtonSelected;
+};
+
+const resetTheGame = () => {
+  
+  roundNumber = 0;
+  playerScore = 0;
+  cpuScore = 0;
+}
+
+//Règle du jeu//
+const gamesRule = (cpuChoice, playerChoice) => {
+  if (cpuChoice === playerChoice) {
+    console.log(`It's a draw, try again !`);
+    roundNumber -= 1;
+  } else if (
+    (cpuChoice === 'rock' && playerChoice === 'scissors') ||
+    (cpuChoice === 'paper' && playerChoice === 'rock') ||
+    (cpuChoice === 'scissors' && playerChoice === 'paper')
+  ) {
+    cpuScore += 1;
+  } else {
+    playerScore += 1;
   }
 };
 
-// 2.Mise en place//
-allButtons.addEventListener('click', (event) => {
-  event.preventDefault();
-
-  if (roundNumber < 5) {
+const gameStartOnClick = (event) => {
+  if (roundNumber >= maximumROund) {
+    playerImage.src = './src/start-screen.png';
+    cpuImage.src = './src/start-screen.png';
+    window.alert(theWinnerIs());
+    resetTheGame();
+  } else {
+    const cpuChoice = cpuSelection();
+    const playerChoice = event.target.id;
+    playerImage.src = imgOfTheSelection[playerChoice];
+    gamesRule(cpuChoice, playerChoice);
     roundNumber += 1;
+
     roundInGame.innerHTML = `Round n°${roundNumber}`;
     score.innerHTML = `Score : CPU ${cpuScore}pt(s) - ${newPlayer.innerHTML} ${playerScore}pt(s)`;
-
-    rockButton.addEventListener('click', (event) => {
-      event.preventDefault();
-
-      const rockImage = document.querySelector('.playerImageChoice');
-      rockImage.src =
-        'https://www.pierrefeuilleciseaux.fr/wp-content/uploads/sites/5/2013/10/pierre-300x300.png';
-      rockImage.alt = 'Player choice: Rock';
-      playerChoiceValue = 'rock';
-      cpuChoiceValue();
-      cpuChoiceValueImg();
-    });
-
-    paperButton.addEventListener('click', (event) => {
-      event.preventDefault();
-
-      const paperImage = document.querySelector('.playerImageChoice');
-      paperImage.src =
-        'https://www.pierrefeuilleciseaux.fr/wp-content/uploads/sites/5/2013/10/feuille-300x300.png';
-      paperImage.alt = 'Player choice: Paper';
-      playerChoiceValue = 'paper';
-      cpuChoiceValue();
-      cpuChoiceValueImg();
-    });
-
-    scissorsButton.addEventListener('click', (event) => {
-      event.preventDefault();
-
-      const scissorsImage = document.querySelector('.playerImageChoice');
-      scissorsImage.src =
-        'https://www.pierrefeuilleciseaux.fr/wp-content/uploads/sites/5/2013/10/ciseaux-300x300.png';
-      scissorsImage.alt = 'Player choice: Scissors';
-      playerChoiceValue = 'scissors';
-      cpuChoiceValue();
-      cpuChoiceValueImg();
-    });
-
-    // 2.1.Fonctionnement du jeu//
-    if (cpuChoiceValue() === 'rock' && playerChoiceValue === 'scissors') {
-      cpuScore += 1;
-      console.log(`CPU : ${cpuScore} point(s)`);
-      console.log(`${newPlayer.innerHTML}: ${playerScore} point(s)`);
-    } else if (cpuChoiceValue() === 'paper' && playerChoiceValue === 'rock') {
-      cpuScore += 1;
-      console.log(`CPU : ${cpuScore} point(s)`);
-      console.log(`${newPlayer.innerHTML}: ${playerScore} point(s)`);
-    } else if (cpuChoiceValue() === 'scissors' && playerChoiceValue === 'paper') {
-      cpuScore += 1;
-      console.log(`CPU : ${cpuScore} point(s)`);
-      console.log(`${newPlayer.innerHTML}: ${playerScore} point(s)`);
-    } else if (cpuChoiceValue() === playerChoiceValue) {
-      console.log(`It's a draw, try again !`);
-      roundNumber -= 1;
-    } else {
-      playerScore += 1;
-      console.log(`${newPlayer.innerHTML}: ${playerScore} point(s)`);
-      console.log(`CPU : ${cpuScore} point(s)`);
-    }
-  } else {
-    alert(`${winner()}`);
   }
-});
+};
+
+//Mise en place//
+for (let i = 0; i < buttonsSelector.length; i += 1) {
+  button = buttonsSelector[i];
+
+  button.addEventListener('click', gameStartOnClick);
+}
